b1f8417d43e8289b2ebfb6ee8c5a24049c3d577c
KwakYounHo <kwakyh951@gmail.com>
2023-04-28 12:18:43 +0900

    덴속의 프로미스는  꼭 다음 덴의 덴으로 추출해야하나?

--- thenTest.js
+++ thenTest.js
@@ -22,15 +22,34 @@
 //     console.log('다섯 번째 덴', value);
 //   })
 
-  const B = new Promise(resolve => {
-    resolve('프로미스 시작~')
+  // const B = new Promise(resolve => {
+    // resolve('프로미스 시작~')
+  // })
+  // .then(value => {
+    // console.log(value);
+    // return new Promise(resolve => {
+      // resolve(['첫 번째 프로미스랑 지금 이 문자열이랑 같이 보내고 싶어~', value]);
+    // })
+  // })
+  // .then(value => {
+    // console.log(value);
+  // })
+
+const C = new Promise(resolve => {
+  resolve('나를 끝까지 전송해 줘~');
+})
+.then(value => {
+  console.log('첫 번째 덴', value)
+  const A = new Promise(resolve => {
+    resolve(value, '나도 버리지 말아줘');
   })
   .then(value => {
-    console.log(value);
-    return new Promise(resolve => {
-      resolve(['첫 번째 프로미스랑 지금 이 문자열이랑 같이 보내고 싶어~', value]);
-    })
+    return value
   })
-  .then(value => {
-    console.log(value);
-  })
+  console.log('덴속의 프로미스', A);
+  return [value, A];
+})
+.then(value => {
+  value[1].then(value => console.log('덴속의 덴',value))
+  console.log('두 번째 덴', value);
+})
