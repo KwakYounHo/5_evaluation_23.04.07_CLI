64c2991c328e919798a80c3f101ff84c02f9684b
KwakYounHo <kwakyh951@gmail.com>
2023-04-28 11:16:19 +0900

    필수 입력 옵션 추가해보기

--- commanderTest.js
+++ commanderTest.js
@@ -3,10 +3,14 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-    .option('-n, --name <name>', 'file name', 'example name')
+    //! requiredOption 메서드는 필수 입력 옵션을 만드는 메서드입니다
+    .requiredOption('-n, --name <name>', 'file name')
     .option('-c, --compress', 'compress', false)
     .parse();
 
 console.log(program.opts().n);
 console.log(program.opts().name);
 console.log(program.opts().compress);
+
+//? 필수 입력 옵션을 입력하지 않으면 에러가 출력 된다.
+//? error: required option '-n, --name <name>' not specified
