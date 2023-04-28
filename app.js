f113cd9034e08d16d27b2b220c78f48f2fbf8656
KwakYounHo <kwakyh951@gmail.com>
2023-04-28 12:46:52 +0900

    inquirer의 return이 promise인 것을 이용해서 async를 사용해서 바꿔봄

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import inquirer from 'inquirer';
 import fs       from 'fs';
 
-inquirer
-  .prompt([
+const A = async ()=>{
+  const A = await inquirer.prompt([
     {
       type: 'input',
       name: 'fileName',
@@ -25,59 +25,58 @@ inquirer
       message: '본문에 입력 될 내용을 입력해 주세요.'
     }
   ])
-  .then(answers => {
-    inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'create',
-        message: `생성될 파일의 확장자는 .html이며\n  생성된 html의 title은 ${answers.title}입니다.\n  ${answers.fileName}이름으로 파일을 생성하시겠습니까?`
-      }
-    ])
-      .then(YorN => {
-        if (YorN.create === true) {
-          //* body에 script태그 안에 넣을 내용
-          let script = ``;
-          
-          //* title태그 추가
-          script += `document.head.innerHTML += '<title>${answers.title}</title>';
-              const mainText = document.createElement('p');
-              mainText.innerText = '${answers.mainText}';
-              `
-          
-          // console.log(answers.rootDiv);
-          //* root div를 만들 것인지
-          if (answers.rootDiv === '네') {
-            script += `const root = document.createElement('div');
-            root.id = 'root';
-            document.body.appendChild(root);
-            root.appendChild(mainText);
-            `
-          } else {
-            script += `document.body.appendChild(mainText);
-            `
-          }
-        
-          //* html의 내용 편집
-          let makeHTML = `<!DOCTYPE html>
-          <html lang="ko">
-          <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          </head>
-          <body>
-            <script>
-              ${script}
-            </script>
-          </body>
-          </html>`
-        
-          //* html 만들기
-          fs.writeFile(`./result/${answers.fileName}.html`, makeHTML, (err, result)=>{
-            if (err) throw err;
-            console.log(result);
-          })
-        }
-      })
-  })
-  // .then(answers => console.log(answers))
+  const B = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'create',
+      message: `생성될 파일의 확장자는 .html이며\n  생성된 html의 title은 ${A.title}입니다.\n  ${A.fileName}이름으로 파일을 생성하시겠습니까?`
+    }
+  ])
+  
+  if (B.create === true) {
+    //* body에 script태그 안에 넣을 내용
+    let script = ``;
+    
+    //* title태그 추가
+    script += `document.head.innerHTML += '<title>${A.title}</title>';
+        const mainText = document.createElement('p');
+        mainText.innerText = '${A.mainText}';
+        `
+
+    // console.log(A.rootDiv);
+    //* root div를 만들 것인지
+    if (A.rootDiv === '네') {
+      script += `const root = document.createElement('div');
+      root.id = 'root';
+      document.body.appendChild(root);
+      root.appendChild(mainText);
+      `
+    } else {
+      script += `document.body.appendChild(mainText);
+      `
+    }
+
+    //* html의 내용 편집
+    let makeHTML = `<!DOCTYPE html>
+    <html lang="ko">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+      <script>
+        ${script}
+      </script>
+    </body>
+    </html>`
+
+    //* html 만들기
+    fs.writeFile(`./result/${A.fileName}.html`, makeHTML, (err, result)=>{
+      if (err) throw err;
+      console.log(result);
+    })
+  }
+}
+
+A();
